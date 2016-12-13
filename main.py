3c0ed0cb13895b13bda92af4d76ec34942afc76a
Chromadude <petersearby8@gmail.com>
2016-12-13 21:46:43 +0000

    adding icon and title

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ SQUARESIZE = 32
 size, legal_moves, starting_board = generator.game(SQUARESIZE)
 g = game.Game([size, legal_moves, SQUARESIZE, starting_board])
 pygame.init()
+pygame.display.set_caption("Cress")
+pygame.display.set_icon(pygame.image.load("icon.png"))
 surface = pygame.display.set_mode((g.board.screenwidth, g.board.screenheight))
 playing = 1
 while playing:
